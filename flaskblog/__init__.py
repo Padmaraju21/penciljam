a12910fe15ac5750a8b1e6e01cb92f219d278e4e
pencil <penciljamnoreply@gmail.com>
2021-01-25 20:57:16 +0000

    master

---
+++ flaskblog/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'penciljamnoreply@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Summer$12'
+mail = Mail(app)
+
+from flaskblog import routes
